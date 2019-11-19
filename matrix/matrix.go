86447d1f04bbfee88291b10bfc505d9a713269be
Kingsley Liao <kingsleyyliao@gmail.com>
2019-11-19 00:43:44 -0500

    Add matrix functionality

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -10,6 +10,16 @@ func ZeroMatrix() Matrix {
 	return Matrix{}
 }
 
+// IdentityMatrix returns a new instance of an identity matrix
+func IdentityMatrix() Matrix {
+	return Matrix{
+		{1, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1},
+	}
+}
+
 // Multiply returns results the result of Matrix being multiplied by another Matrix
 func (m Matrix) Multiply(a Matrix) Matrix {
 	result := Matrix{}
@@ -41,4 +51,43 @@ func (m Matrix) MultiplyTuple(t tuple.Tuple) tuple.Tuple {
 	}
 
 	return tuple.Tuple{X: result[0], Y: result[1], Z: result[2], W: result[3]}
+}
+
+// Transpose returns the transposed form of a matrix
+func (m Matrix) Transpose() Matrix {
+	transposed := Matrix{}
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			transposed[i][j] = m[j][i]
+		}
+	}
+
+	return transposed
+}
+
+// Determinant returns the determinant of a 2x2 matrix
+func Determinant(m [2][2]float64) float64 {
+	return m[0][0]*m[1][1] - m[0][1]*m[1][0]
+}
+
+// SubMatrix returns the submatrix of a matrix by ignoring row r and column c
+func SubMatrix(m Matrix, r, c int) [][]float64 {
+	result := [][]float64{}
+	rowOffset := 0
+
+	for i := 0; i < len(m); i++ {
+		if i == r {
+			rowOffset = 1
+			continue
+		}
+		row := append(m[i][0:c], m[i][c+1:]...)
+		result = append(result, row)
+		for j := range row {
+			result[i-rowOffset][j] = row[j]
+		}
+
+	}
+
+	return result
 }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -20,6 +20,41 @@ func TestZeroMatrix(t *testing.T) {
 	}
 }
 
+func TestIdentityMatrix(t *testing.T) {
+	m1 := IdentityMatrix()
+	expected := Matrix{
+		{1, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1},
+	}
+
+	if m1 != expected {
+		t.Errorf("expected %v, got %v", expected, m1)
+	}
+}
+
+func TestIdentityMatrixMultiplication(t *testing.T) {
+	m1 := Matrix{
+		{1, 2, 3, 4},
+		{1, 2, 4, 8},
+		{2, 4, 8, 16},
+		{4, 8, 16, 32},
+	}
+
+	result := m1.Multiply(IdentityMatrix())
+	expected := Matrix{
+		{1, 2, 3, 4},
+		{1, 2, 4, 8},
+		{2, 4, 8, 16},
+		{4, 8, 16, 32},
+	}
+
+	if result != expected {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+}
+
 func TestMatrixEquality(t *testing.T) {
 	m1 := Matrix{
 		{1, 2, 3, 4},
@@ -86,4 +121,83 @@ func TestMatrixTupleMultiplication(t *testing.T) {
 	if !result.Equals(expected) {
 		t.Errorf("expected %v, got %v", expected, result)
 	}
+}
+
+func TestMatrixTranspose(t *testing.T) {
+	m1 := Matrix{
+		{0, 9, 3, 0},
+		{9, 8, 0, 8},
+		{1, 8, 5, 3},
+		{0, 0, 5, 8},
+	}
+
+	transposed := m1.Transpose()
+	expected := Matrix{
+		{0, 9, 1, 0},
+		{9, 8, 8, 0},
+		{3, 0, 5, 5},
+		{0, 8, 3, 8},
+	}
+
+	if transposed != expected {
+		t.Errorf("expected %v, got %v", expected, transposed)
+	}
+}
+
+func TestTransposeIdentityMatrix(t *testing.T) {
+	expected := IdentityMatrix()
+	transposed := expected.Transpose()
+
+	if transposed != expected {
+		t.Errorf("expected %v, got %v", expected, transposed)
+	}
+}
+
+func TestDeterminant(t *testing.T) {
+	m := [2][2]float64{
+		{1, 5},
+		{-3, 2},
+	}
+
+	d := Determinant(m)
+	var expected float64 = 17
+
+	if d != expected {
+		t.Errorf("expected %v, got %v", expected, d)
+	}
+}
+
+func TestSubMatrix(t *testing.T) {
+	m := Matrix{
+		{-6, 1, 1, 6},
+		{-8, 5, 8, 6},
+		{-1, 0, 8, 2},
+		{-7, 1, -1, 1},
+	}
+
+	sub := SubMatrix(m, 2, 1)
+	expected := [][]float64{
+		{-6, 1, 6},
+		{-8, 8, 6},
+		{-7, -1, 1},
+	}
+
+	if !isEqual(sub, expected) {
+		t.Errorf("expected %v, got %v", expected, sub)
+	}
+
+}
+
+func isEqual(a, b [][]float64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		for j := range a[i] {
+			if a[i][j] != b[i][j] {
+				return false
+			}
+		}
+	}
+	return true
 }
