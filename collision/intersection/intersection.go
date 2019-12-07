9ccf981e265997ee03eaffbf243bd20863a13eb4
Kingsley Liao <kingsleyyliao@gmail.com>
2019-12-07 18:35:02 -0500

    Change Intersect func to return Intersection instead of raw T value

--- collision/intersection/intersection.go
+++ collision/intersection/intersection.go
@@ -7,8 +7,31 @@ import (
 	"github.com/kingsleyliao/ray-tracer/rendering/shape"
 )
 
+// Intersection encapsulates t and object
+type Intersection struct {
+	T      float64
+	Object shape.Sphere
+}
+
+// NewIntersection is the constructor for Intersection
+func NewIntersection(t float64, s shape.Sphere) Intersection {
+	return Intersection{
+		T:      t,
+		Object: s,
+	}
+}
+
+// Intersections creates a slice of Intersection
+func Intersections(xs ...Intersection) []Intersection {
+	intersections := make([]Intersection, 0)
+	for _, x := range xs {
+		intersections = append(intersections, x)
+	}
+	return intersections
+}
+
 // Intersect finds the intersection points (time T) of a ray and a sphere
-func Intersect(s shape.Sphere, r ray.Ray) []float64 {
+func Intersect(s shape.Sphere, r ray.Ray) []Intersection {
 	sphereToRay := r.Origin.Subtract(s.Center)
 
 	a := r.Direction.Dot(r.Direction)
@@ -17,10 +40,14 @@ func Intersect(s shape.Sphere, r ray.Ray) []float64 {
 
 	discrminant := (b * b) - (4 * a * c)
 	if discrminant < 0 {
-		return nil
+		return []Intersection{}
 	}
 
 	t1 := (-b - math.Sqrt(discrminant)) / (2 * a)
 	t2 := (-b + math.Sqrt(discrminant)) / (2 * a)
-	return []float64{t1, t2}
+
+	i1 := Intersection{T: t1, Object: s}
+	i2 := Intersection{T: t2, Object: s}
+
+	return []Intersection{i1, i2}
 }
--- collision/intersection/intersection_test.go
+++ collision/intersection/intersection_test.go
@@ -9,6 +9,19 @@ import (
 	"github.com/kingsleyliao/ray-tracer/rendering/shape"
 )
 
+// An Intersection encapsulates t and object
+func TestIntersection_1(t *testing.T) {
+	s := shape.NewSphere()
+	i := NewIntersection(3.5, s)
+
+	expectedT := 3.5
+	expectedS := s
+
+	if expectedT != i.T && i.Object != expectedS {
+		t.Errorf("expected %v, %v, got %v, %v", expectedT, expectedS, i.T, i.Object)
+	}
+}
+
 // A ray intersects a sphere at two points
 func TestIntersect_1(t *testing.T) {
 	r := ray.NewRay(point.NewPoint(0, 0, -5), vector.NewVector(0, 0, 1))
@@ -16,8 +29,8 @@ func TestIntersect_1(t *testing.T) {
 
 	xs := Intersect(s, r)
 
-	expected0 := 4.0 == xs[0]
-	expected1 := 6.0 == xs[1]
+	expected0 := s == xs[0].Object
+	expected1 := s == xs[1].Object
 
 	if expected0 != true || expected1 != true {
 		t.Errorf("expected %v, %v, got %v, %v", expected0, expected1, xs[0], xs[1])
@@ -31,8 +44,8 @@ func TestIntersect_2(t *testing.T) {
 
 	xs := Intersect(s, r)
 
-	expected0 := 5.0 == xs[0]
-	expected1 := 5.0 == xs[1]
+	expected0 := s == xs[0].Object
+	expected1 := s == xs[1].Object
 	expected2 := len(xs) == 2
 
 	if expected0 != true || expected1 != true || expected2 != true {
@@ -63,8 +76,8 @@ func TestIntersect_4(t *testing.T) {
 	xs := Intersect(s, r)
 
 	expected := len(xs) == 2
-	expected1 := -1.0 == xs[0]
-	expected2 := 1.0 == xs[1]
+	expected1 := s == xs[0].Object
+	expected2 := s == xs[1].Object
 
 	if expected != true || expected1 != true || expected2 != true {
 		t.Errorf("expected %v, %v, %v got %v, %v, %v", expected, expected1, expected2, len(xs), xs[0], xs[1])
@@ -79,8 +92,8 @@ func TestIntersect_5(t *testing.T) {
 	xs := Intersect(s, r)
 
 	expected := len(xs) == 2
-	expected1 := -6.0 == xs[0]
-	expected2 := -4.0 == xs[1]
+	expected1 := s == xs[0].Object
+	expected2 := s == xs[1].Object
 
 	if expected != true || expected1 != true || expected2 != true {
 		t.Errorf("expected %v, %v, %v got %v, %v, %v", expected, expected1, expected2, len(xs), xs[0], xs[1])
