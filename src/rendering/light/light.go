587b5ea9bf08a9c1cf72abc48ba88576b6ed8b1c
Kingsley Liao <kingsleyyliao@gmail.com>
2019-12-11 00:13:40 -0500

    Add comments to lighting shader algorithm

--- src/rendering/light/light.go
+++ src/rendering/light/light.go
@@ -25,30 +25,41 @@ func NewPointLight(p point.Point, i color.Color) Light {
 
 // Lighting returns a Color based on the Lighting
 func Lighting(m material.Material, l Light, p point.Point, eyeV, normalV vector.Vector) color.Color {
+	// Combine the surface color witht he light's color and intensity
 	effectiveColor := m.Color.Multiply(l.Intensity)
+
+	// Find the direction of the light source with respect to p (usually zero with unit spheres)
 	lightVector := l.Position.Subtract(p).Normalize()
+
+	// Compute the ambient contribution (default ambient is 0.1)
 	ambient := effectiveColor.Scale(m.Ambient)
 
+	// lightDotNormal represents the cosine of the angle between the light vector and the normal vector.
 	lightDotNormal := lightVector.Dot(normalV)
 
 	var diffuse color.Color
 	var specular color.Color
 	if lightDotNormal < 0 {
+		// A negative number means the light is on the opposite side of the surface
 		diffuse = color.Black()
 		specular = color.Black()
 	} else {
+		// compute the diffuse contribution
 		diffuse = effectiveColor.Scale(m.Diffuse).Scale(lightDotNormal)
-
 		reflectV := lightVector.Invert().Reflect(normalV)
+
+		// reflectiveDotEye represents the cosine of the angle between the reflection vector and the eye vector
 		reflectDotEye := reflectV.Dot(eyeV)
 
 		if reflectDotEye <= 0 {
+			// A negative number means the light reflects away from the eye
 			specular = color.Black()
 		} else {
 			factor := math.Pow(reflectDotEye, m.Shininess)
+			// Compute the specular contribution
 			specular = l.Intensity.Scale(m.Specular * factor)
 		}
 	}
-
+	// Add all three contributions for the final shading
 	return ambient.Add(diffuse).Add(specular)
 }
